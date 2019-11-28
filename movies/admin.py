318f21e397b07d40137cd613cfa3a0e1d9bf84f2
banggeut01 <genie121110@gmail.com>
2019-11-28 18:08:32 +0900

    Admin

--- movies/admin.py
+++ movies/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Movie, Genre, Review
+from .models import Movie, Genre, Review, State, Wish
 # Register your models here.
 
 admin.site.register(Movie)
 admin.site.register(Review)
 admin.site.register(Genre)
-
+admin.site.register(State)
+admin.site.register(Wish)
