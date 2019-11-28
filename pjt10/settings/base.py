b7e5adb59f377dd6047658b72b9290737e2de65b
banggeut01 <genie121110@gmail.com>
2019-11-28 09:50:45 +0900

    Add staticfiles setting, pip

--- pjt10/settings/base.py
+++ pjt10/settings/base.py
@@ -133,6 +133,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 ) 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # 배포 
 
 AUTH_USER_MODEL = 'accounts.User'
 
